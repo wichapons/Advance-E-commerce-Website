e1898f544cffa6a4bc288b6aec2e2b8a8cb7dfa3
Wichapon V <wichapon@gmail.com>
2023-05-13 15:43:32 +0700

    Update ProtectedRoutesComponents to handle authorization for admins and users

--- frontend/src/components/ProtectedRoutesComponents.jsx
+++ frontend/src/components/ProtectedRoutesComponents.jsx
@@ -1,9 +1,21 @@
 import { Outlet,Navigate } from "react-router-dom";
 
-const ProtectedRoutesComponents = ()=>{
-    const auth = true;
-    console.log(auth);
+const ProtectedRoutesComponents = ({admin})=>{
+    let auth;
+    if(admin){
+        let adminAuth = true;
+        if (adminAuth){
+            auth = true;
+        }
+    }else{
+        let userAuth = true;
+        if(userAuth){
+            auth = true;
+        }
+    }
+
     return auth ? <Outlet /> : <Navigate to='/login'/> //Outlet is the route behind ProtectedRoutesComponents
+
 }
 
 export default ProtectedRoutesComponents;
