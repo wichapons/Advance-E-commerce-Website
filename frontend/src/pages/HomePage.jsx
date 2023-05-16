6603d96b79298b46d615001e448a00e9610a7a5c
Wichapon V <wichapon@gmail.com>
2023-05-16 23:07:52 +0700

    Add ProductCarouselComponent, CategoryCardComponent, and images to HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,42 @@
+import ProductCarouselComponent from "../components/ProductCarouselComponent";
+import CategoryCardComponent from "../components/CategoryCardComponent";
+import { Container,Row } from "react-bootstrap";
 
-const HomePage = ()=>{
-    return (
-        <p>This is a homepage aaa</p>
-    )
-}
+const HomePage = () => {
+  const categories = [
+    "Tablets",
+    "Monitors",
+    "Games",
+    "Printers",
+    "Software",
+    "Cameras",
+    "Books",
+    "Videos",
+  ];
+
+  const images = [
+    "/images/tablets-category.png",
+    "/images/monitors-category.png",
+    "/images/games-category.png",
+    "/images/tablets-category.png",
+    "/images/tablets-category.png",
+    "/images/tablets-category.png",
+    "/images/tablets-category.png",
+    "/images/tablets-category.png",
+  ];
+
+  return (
+    <>
+      <ProductCarouselComponent />
+      <Container>
+        <Row xs={1} md={2} className="g-4 mt-4" >
+          {categories.map((category,index) => (
+            <CategoryCardComponent key={index} category={categories[index]} src={images[index]}  />
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export default HomePage;
-
-
