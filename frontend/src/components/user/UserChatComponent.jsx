fbbe4e4f63b0b28ccf0c8331dde4612d9f9c169f
Wichapon V <wichapon@gmail.com>
2023-05-17 23:13:49 +0700

    Add UserChatComponent to render a chat interface

---
+++ frontend/src/components/user/UserChatComponent.jsx
@@ -0,0 +1,45 @@
+import "../../chat.css";
+
+const UserChatComponent = () => {
+  return (
+    <>
+      <input type="checkbox" id="checkbox" />
+      <label className="chat-btn" htmlFor="checkbox">
+        <i className="bi bi-chat-dots comment"></i>
+        <span className="position-absolute top-0 start-10 translate-middle p-2 bg-danger border border-light rounded-circle"></span>
+        <i className="bi bi-x-circle close"></i>
+      </label>
+      <div className="chat-wrapper">
+        <div className="chat-header">
+          <h6>Let's Chat - Online</h6>
+        </div>
+        <div className="chat-form">
+        <div className="cht-msg">
+            {Array.from({ length: 20 }).map((_, id) => {
+              return(
+                <div key={id}>
+                <p>
+                  <b>You wrote:</b> Hello, world! This is a toast message.
+                </p>
+                <p className="bg-primary p-3 ms-4 text-light rounded-pill">
+                  <b>Support wrote:</b> Hello, world! This is a toast message.
+                </p>
+              </div>
+              )
+            }
+            )}
+          </div>
+          <textarea
+            id="clientChatMsg"
+            className="form-control"
+            placeholder="Your Text Message"
+          ></textarea>
+
+          <button className="btn btn-success btn-block">Submit</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserChatComponent;
