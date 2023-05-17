0b07e1958270fc44c4999be8358d219634525aed
Wichapon V <wichapon@gmail.com>
2023-05-17 23:14:05 +0700

    Add ProductListPage component with filtering options

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,7 +1,47 @@
-const ProductPage = ()=>{
-    return (
-        <p>This is a Login aaa</p>
-    )
-}
+import { Row, Col, Container, ListGroup, Button } from "react-bootstrap";
+import PaginationComponent from "../components/PaginationComponent";
+import ProductForListComponent from "../components/ProductForListComponent";
+import SortOptionsComponent from "../components/SortOptionsComponent";
+import PriceFilterComponent from "../components/filterQueryResultOptions/PriceFilterComponent";
+import RatingFilterComponent from "../components/filterQueryResultOptions/RatingFilterComponent";
+import CategoryFilterComponent from "../components/filterQueryResultOptions/CategoryFilterComponent";
+import AttributesFilterComponent from "../components/filterQueryResultOptions/AttributesFilterComponent.jsx";
 
-export default ProductPage;
+const ProductListPage = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item className="mb-2 mt-2"><SortOptionsComponent /></ListGroup.Item>
+            <ListGroup.Item><PriceFilterComponent /></ListGroup.Item>
+            <ListGroup.Item><RatingFilterComponent /></ListGroup.Item>
+            <ListGroup.Item><CategoryFilterComponent /></ListGroup.Item>
+            <ListGroup.Item>
+              <AttributesFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button variant="primary">Filter</Button>
+              <Button variant="danger">Reset Filter</Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={9}>
+          {Array.from({length:5}).map((_,index)=>{
+            return(
+              <ProductForListComponent key={index}
+              images={["games","monitors","tablets","games","monitors"]}
+              index={index}
+              />
+            )
+          })}
+          <PaginationComponent />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProductListPage;
+
+
