9070bcca30532c821bce004685a502fd2ede9522
Wichapon V <wichapon@gmail.com>
2023-05-17 14:36:51 +0700

    Add footer component to page

--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -1,5 +1,19 @@
-const FooterComponent = () => {
-    return <p>This is a footer</p>
-}
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-export default FooterComponent
+const FooterComponent = () => {
+  return (
+    <footer>
+      <Container fluid>
+        <Row>
+          <Col className="bg-dark text-white text-center py-3">
+            Copyright &copy; 2023 Topazio Shop
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default FooterComponent;
