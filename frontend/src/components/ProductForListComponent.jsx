7ac25166523abfed1b0ac8e63f29571b8c26fda2
Wichapon V <wichapon@gmail.com>
2023-05-17 23:14:18 +0700

    Create ProductForListComponent component

---
+++ frontend/src/components/ProductForListComponent.jsx
@@ -0,0 +1,36 @@
+import { Card, Button, Col, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Rating } from "react-simple-star-rating";
+
+const ProductForListComponent = (props) => {
+  return (
+    <Card style={{ marginTop: "30px", marginBottom: "40px" }}>
+      <Row>
+        <Col lg={5}>
+          <Card.Img variant="top" src={"/images/"+props.images[props.index]+"-category.png"} />
+        </Col>
+
+        <Col lg={7}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Card.Text>
+              <Rating readonly initialValue={5} size={20} /> (1)
+            </Card.Text>
+            <Card.Text className="h4" >
+              $12 {' '}
+              <LinkContainer to="/product-details">
+              <Button variant="danger">Detail</Button>
+              </LinkContainer>
+            </Card.Text>
+          </Card.Body>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+
+export default ProductForListComponent;
