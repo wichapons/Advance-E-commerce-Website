d8f5e3206d9e422a5ba1a9590eb2766eca3a427e
Wichapon V <wichapon@gmail.com>
2023-05-20 17:31:36 +0700

    Update main.jsx to include Bootstrap and Bootstrap-icons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css'
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
