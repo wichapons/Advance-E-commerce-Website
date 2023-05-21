ccd5aa2b1bd0d2cb721f2463c60e65b339c1ac16
Wichapon V <wichapon@gmail.com>
2023-05-21 16:01:50 +0700

    Add cart functionality to CartPage

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,7 +1,48 @@
-const CartPage = ()=>{
-    return (
-        <p>This is a cart page</p>
-    )
-}
+import { Container, Row, Col, Alert, ListGroup, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import CartItemComponent from "../components/CartItemComponent";
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <Container fluid>
+      <Row className="mt-4">
+        {/*Shopping Cart column*/}
+        <Col md={8} >
+          <h1>Shopping Cart</h1>
+          <ListGroup variant="flush">
+          {Array.from({ length: 3 }).map((item,idx) => (
+            <React.Fragment key={idx}> 
+              <CartItemComponent />
+              <br></br>
+            </React.Fragment>
+          ))}
+          </ListGroup>
+          
+          <Alert variant="info">Your cart is empty</Alert>
+        </Col>
+
+        {/* Total Product Column */}
+        <Col md={4}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h3>Subtotal (2 Items)</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Price: <span className="fw-bold">$892</span>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <LinkContainer to="/user/order-details">
+                <Button type="button">Proceed To Checkout</Button>
+              </LinkContainer>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default CartPage;
