b05de9b31ba2d934300ada7b354b0edd2aac74a9
Wichapon V <wichapon@gmail.com>
2023-05-23 20:17:38 +0700

    Add UserChatComponent to ProtectedRoutesComponents

--- frontend/src/components/ProtectedRoutesComponents.jsx
+++ frontend/src/components/ProtectedRoutesComponents.jsx
@@ -1,20 +1,16 @@
 import { Outlet,Navigate } from "react-router-dom";
+import UserChatComponent from "./user/UserChatComponent";
 
 const ProtectedRoutesComponents = ({admin})=>{
-    let auth;
     if(admin){
         let adminAuth = true;
-        if (adminAuth){
-            auth = true;
-        }
+        return adminAuth ? <Outlet /> : <Navigate to='/login'/> //Outlet is the route behind ProtectedRoutesComponents
     }else{
         let userAuth = true;
-        if(userAuth){
-            auth = true;
-        }
+        //return 2 react object which are routes behind this component and chat box
+        return userAuth ? <> <Outlet /> <UserChatComponent/> </> : <Navigate to='/login'/>  
     }
 
-    return auth ? <Outlet /> : <Navigate to='/login'/> //Outlet is the route behind ProtectedRoutesComponents
 
 }
 
