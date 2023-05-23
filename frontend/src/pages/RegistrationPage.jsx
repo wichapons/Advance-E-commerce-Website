c2033cb3fc2d5f37e363d79a1cd5afddcfee7bbb
Wichapon V <wichapon@gmail.com>
2023-05-23 16:02:13 +0700

    add logic for check password matched

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -1,117 +1,168 @@
-import { Container,Row,Col,Form,Button,Spinner,Alert } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Spinner from 'react-bootstrap/Spinner'
 
+/*
+TO BE DONE IN FUTURE
 const RegistrationPage = () => {
     const [validated, setValidated] = useState(false);
-  
-    const handleSubmit = (event) => {
-      const form = event.currentTarget;
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      setValidated(true);
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    //const [passwordMatchError, setPasswordMatchError] = useState(false);
+
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+      setPasswordMatchError(false);
     };
-    return (
-      <Container>
-        <Row className="mt-5 justify-content-md-center">
-          <Col md={6}>
-            <h1>Register</h1>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
-              <Form.Group className="mb-3" controlId="validationCustom01">
-                <Form.Label>Your first name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="Enter your first name"
-                  name="firstname"
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please enter your first name
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicLastName">
-                <Form.Label>Your last name</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="Enter your last name"
-                  name="lastName"
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please enter your last name
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  name="email"
-                  required
-                  type="email"
-                  placeholder="Enter email"
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please anter a valid email address
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  name="password"
-                  required
-                  type="password"
-                  placeholder="Password"
-                  minLength={6}
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please anter a valid password
-                </Form.Control.Feedback>
-                <Form.Text className="text-muted">
-                  Password should have at least 6 characters
-                </Form.Text>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPasswordRepeat">
-                <Form.Label>Repeat Password</Form.Label>
-                <Form.Control
-                  name="confirmPassword"
-                  required
-                  type="password"
-                  placeholder="Repeat Password"
-                  minLength={6}
-                />
-                <Form.Control.Feedback type="invalid">
-                  Both passwords should match
-                </Form.Control.Feedback>
-              </Form.Group>
-  
-              <Row className="pb-2">
-                <Col>
-                  Do you have an account already?
-                  <Link to={"/login"}> Login </Link>
-                </Col>
-              </Row>
-  
-              <Button type="submit">
-                <Spinner
-                  as="span"
-                  animation="border"
-                  size="sm"
-                  role="status"
-                  aria-hidden="true"
-                />
-                Submit
-              </Button>
-              <Alert show={true} variant="danger">
-                  User with that email already exists!
-              </Alert>
-              <Alert show={true} variant="info">
-                  User created
-              </Alert>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
+
+    const handleConfirmPasswordChange = (event)=>{
+      setConfirmPassword(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log('submited received');
+      const value = event.target.value;
+      
+      // Check for at least one uppercase letter, one lowercase letter, and one number
+      const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
+      const isValid = regex.test(value);
+
+      // Check for special characters
+      const specialChars = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/;
+      const hasSpecialChars = specialChars.test(value);
+
+      if (password.length >= 6 && isValid && !hasSpecialChars ){
+        setValidated(true);
+      }else{
+        setPasswordMatchError(true);
+      }
+      if (confirmPassword === password) {
+        setValidated(true);
+        // Perform further form submission or validation logic
+      } else {
+        setPasswordMatchError(true);
+      }
+    }
+*/
+
+
+const RegistrationPage = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
   };
+  return (
+    <Container>
+      <Row className="mt-5 justify-content-md-center">
+        <Col md={6}>
+          <h1>Register</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="validationCustom01">
+              <Form.Label>Your name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter your name"
+                name="name"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter a name
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicLastName">
+              <Form.Label>Your last name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter your last name"
+                name="lastName"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter your last name
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                name="email"
+                required
+                type="email"
+                placeholder="Enter email"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please anter a valid email address
+              </Form.Control.Feedback>
+            </Form.Group>
+             {/* Password Input */}
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name="password"
+                required
+                type="password"
+                placeholder="Password"
+                minLength={6}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please anter a valid password
+              </Form.Control.Feedback>
+              <Form.Text className="text-muted">
+                Password should have at least 6 characters
+              </Form.Text>
+            </Form.Group>
+            {/* Password confirmation input */}
+            <Form.Group className="mb-3" controlId="formBasicPasswordRepeat">
+              <Form.Label>Repeat Password</Form.Label>
+              <Form.Control
+                name="confirmPassword"
+                required
+                type="password"
+                placeholder="Repeat Password"
+                minLength={6}
+              />
+              <Form.Control.Feedback type="invalid">
+                Both passwords should match
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Row className="pb-2">
+              <Col>
+                Do you have an account already?
+                <Link to={"/login"}> Login </Link>
+              </Col>
+            </Row>
+
+            <Button type="submit">
+              <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              Submit
+            </Button>
+            <Alert show={true} variant="danger">
+                User with that email already exists!
+            </Alert>
+            <Alert show={true} variant="info">
+                User created
+            </Alert>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
 export default RegistrationPage;
+
