446ccac95dc233a79d65f68f2007aecf88f64521
Wichapon V <wichapon@gmail.com>
2023-05-23 16:03:53 +0700

    Add success alert and buttons for added to cart message

--- frontend/src/components/AddedToCartMessageComponent.jsx
+++ frontend/src/components/AddedToCartMessageComponent.jsx
@@ -1,16 +1,25 @@
-import { Alert } from "react-bootstrap";
+import { Alert, Button } from "react-bootstrap";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const AddedToCartMessageComponent = () => {
   const [show, setShow] = useState(true);
   return (
-    <Alert show={show} variant="danger" onClose={() => setShow(false)} dismissible>
+    <Alert
+      show={show}
+      variant="success"
+      onClose={() => setShow(false)}
+      dismissible
+    >
       <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-      <p>
-        Change this and that and try again. Duis mollis, est non commodo luctus,
-        nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis
-        consectetur purus sit amet fermentum.
-      </p>
+      <p>The product has been added to cart successfully.</p>
+      <Link to="/home">
+        <Button className="me-2" variant="success">Back to homepage</Button>
+      </Link>
+
+      <Link to="/cart">
+        <Button variant="danger">Go to cart</Button>
+      </Link>
     </Alert>
   );
 };
