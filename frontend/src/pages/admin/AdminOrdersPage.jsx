02485af0738eb3436b08a000d4415e6080bdbcc7
Wichapon V <wichapon@gmail.com>
2023-05-24 22:22:33 +0700

    Add table to AdminOrdersPage to display order details

--- frontend/src/pages/admin/AdminOrdersPage.jsx
+++ frontend/src/pages/admin/AdminOrdersPage.jsx
@@ -1,5 +1,44 @@
+import { Row, Col, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const AdminOrdersPage = () => {
-  return <p>This is an orders page</p>;
+  return (
+    <Row className="m-5">
+      <Col md={2}>
+      Admin links
+      </Col>
+      <Col md={10}>
+        <h1>My Orders</h1>
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User</th>
+              <th>Date</th>
+              <th>Total</th>
+              <th>Delivered</th>
+              <th>Payment method</th>
+              <th>Order details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map(
+              (item, idx) => (
+                <tr key={idx}>
+                  <td>{idx +1}</td>
+                  <td>Mark Twain</td>
+                  <td>2022-09-12</td>
+                  <td>$124</td>
+                  <td><i className={item}></i></td>
+                  <td>Paypal</td>
+                  <td><Link to="/admin/order-details">go to order</Link></td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
 };
 
 export default AdminOrdersPage;
