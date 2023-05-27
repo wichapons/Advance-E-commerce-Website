8749bcb82bc2aa5cfbf2a3601d85eebfca26c524
Wichapon V <wichapon@gmail.com>
2023-05-27 21:49:29 +0700

    Add react-bootstrap components and alert to AdminCreateProductPage

--- frontend/src/pages/admin/AdminCreateProductPage.jsx
+++ frontend/src/pages/admin/AdminCreateProductPage.jsx
@@ -1,4 +1,4 @@
-import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import {Row,Col,Container,Form,Button,CloseButton,Table,Alert} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
@@ -8,7 +8,7 @@ const AdminCreateProductPage = () => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
-      event.stopPropagation();
+      event.stopPropagation()
     }
 
     setValidated(true);
@@ -16,24 +16,21 @@ const AdminCreateProductPage = () => {
   return (
     <Container>
       <Row className="justify-content-md-center mt-5">
-        <Col  className="d-flex justify-content-end"  md={2} >
-          <Link to="/admin/products"> 
-          <Button >Go Back</Button>
-            
+        <Col md={1}>
+          <Link to="/admin/products" className="btn btn-info my-3">
+            Go Back
           </Link>
         </Col>
+
         <Col md={6}>
-          <h3>Create a new product</h3>
+          <h1>Create a new product</h1>
           <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label>Name</Form.Label>
               <Form.Control name="name" required type="text" />
             </Form.Group>
 
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
               <Form.Label>Description</Form.Label>
               <Form.Control
                 name="description"
@@ -51,18 +48,100 @@ const AdminCreateProductPage = () => {
               <Form.Control name="price" required type="text" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCategory">
-              <Form.Label>Category</Form.Label>
-              <Form.Select
-                required
-                name="category"
-                aria-label="Default select example"
-              >
+              <Form.Label>
+                Category
+                <CloseButton />(<small>remove selected</small>)
+              </Form.Label>
+              <Form.Select required name="category" aria-label="Default select example">
                 <option value="">Choose category</option>
                 <option value="1">Laptops</option>
                 <option value="2">TV</option>
                 <option value="3">Games</option>
               </Form.Select>
             </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicNewCategory">
+              <Form.Label>
+                Or create a new category (e.g. Computers/Laptops/Intel){" "}
+              </Form.Label>
+              <Form.Control name="newCategory" type="text" />
+            </Form.Group>
+
+            <Row className="mt-5">
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formBasicAttributes">
+                  <Form.Label>Choose atrribute and set value</Form.Label>
+                  <Form.Select name="atrrKey" aria-label="Default select example">
+                    <option>Choose attribute</option>
+                    <option value="red">color</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formBasicAttributeValue">
+                  <Form.Label>Attribute value</Form.Label>
+                  <Form.Select name="atrrVal" aria-label="Default select example">
+                    <option>Choose attribute value</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Row>
+              <Table hover>
+                <thead>
+                  <tr>
+                    <th>Attribute</th>
+                    <th>Value</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>attr key</td>
+                    <td>attr value</td>
+                    <td>
+                      <CloseButton />
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Row>
+
+            <Row>
+              <Col md={6}>
+                <Form.Group className="mb-3" controlId="formBasicNewAttribute">
+                  <Form.Label>Create new attribute</Form.Label>
+                  <Form.Control
+                    disabled={false}
+                    placeholder="choose or create category"
+                    name="newAttrValue"
+                    type="text"
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group
+                  className="mb-3"
+                  controlId="formBasicNewAttributeValue"
+                >
+                  <Form.Label>Attribute value</Form.Label>
+                  <Form.Control
+                    disabled={false}
+                    placeholder="first choose or create category"
+                    required={true}
+                    name="newAttrValue"
+                    type="text"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Alert variant="primary">
+              After typing attribute key and value press enter on one of the
+              field
+            </Alert>
+
             <Form.Group controlId="formFileMultiple" className="mb-3 mt-3">
               <Form.Label>Images</Form.Label>
 
@@ -79,6 +158,4 @@ const AdminCreateProductPage = () => {
 };
 
 export default AdminCreateProductPage;
-
-
 
