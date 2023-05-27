9fdf38910758170f1209679a56b50cda0ecf67a6
Wichapon V <wichapon@gmail.com>
2023-05-27 20:18:57 +0700

    Add Form for creating new product

--- frontend/src/pages/admin/AdminCreateProductPage.jsx
+++ frontend/src/pages/admin/AdminCreateProductPage.jsx
@@ -1,6 +1,84 @@
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const AdminCreateProductPage = () => {
-  return <p>This is a create product page</p>;
+  const [validated, setValidated] = useState(false);
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+  return (
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col  className="d-flex justify-content-end"  md={2} >
+          <Link to="/admin/products"> 
+          <Button >Go Back</Button>
+            
+          </Link>
+        </Col>
+        <Col md={6}>
+          <h3>Create a new product</h3>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control name="name" required type="text" />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                name="description"
+                required
+                as="textarea"
+                rows={3}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCount">
+              <Form.Label>Count in stock</Form.Label>
+              <Form.Control name="count" required type="number" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control name="price" required type="text" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCategory">
+              <Form.Label>Category</Form.Label>
+              <Form.Select
+                required
+                name="category"
+                aria-label="Default select example"
+              >
+                <option value="">Choose category</option>
+                <option value="1">Laptops</option>
+                <option value="2">TV</option>
+                <option value="3">Games</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group controlId="formFileMultiple" className="mb-3 mt-3">
+              <Form.Label>Images</Form.Label>
+
+              <Form.Control required type="file" multiple />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Create
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default AdminCreateProductPage;
+
+
 
