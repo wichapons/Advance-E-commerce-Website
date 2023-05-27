7854c2cce945171ae8042702ca9fb4880039d087
Wichapon V <wichapon@gmail.com>
2023-05-27 22:10:20 +0700

    initial commit

--- frontend/src/pages/admin/AdminEditUserPage.jsx
+++ frontend/src/pages/admin/AdminEditUserPage.jsx
@@ -1,6 +1,59 @@
+import {Row,Col,Container,Form,Button,Table} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const AdminEditUserPage = () => {
-  return <p>This is an edit user page</p>;
+  const [validated, setValidated] = useState(false);
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
+
+  return (
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col md={1}>
+          <Link to="/admin/products" className="btn btn-info my-3">
+            Go Back
+          </Link>
+        </Col>
+
+        <Col md={6}>
+          <h1>Edit user details</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control name="name" required type="text" defaultValue="" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control name="lastName" required type="text" defaultValue= ""/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCount">
+              <Form.Label>Email</Form.Label>
+              <Form.Control name="email" required type="email" defaultValue="" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check name="isAdmin" type="checkbox" label="Is admin" />
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+              Update
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default AdminEditUserPage;
+
+
 
