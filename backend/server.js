ae597e7835d040a6f763805008ee439dca88aebe
Wichapon V <wichapon@gmail.com>
2023-05-31 00:35:28 +0700

    Add Product Model and create new product in '/' endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const app = express()
 const mongoose = require('mongoose')
 const connectDB = require('./config/db')
 //Model
+const Product = require('./models/ProductModel')
 
 //config
 require('dotenv').config();
@@ -14,9 +15,16 @@ const port = 5000
 
 const apiRoutes = require("./routes/apiRoutes")
 
-app.get('/', (req, res) => {
-
-    res.json({message: "API running..."})
+app.get('/',async (req, res,next) => {
+  try{
+    const product = new Product;
+    product.name = "PC";
+    await product.save();
+    const products = await Product.find();
+    res.send(`product has been create for ${products.length} ${product.id}`)
+  }catch(err){
+    next(err)
+  }
 })
 
 app.use('/api', apiRoutes)
