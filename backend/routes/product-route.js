b55cf55790e81b48b4ce7b61059afd2949c2be4f
Wichapon V <wichapon@gmail.com>
2023-06-03 15:38:41 +0700

    Remove product route file

--- backend/routes/product-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get("/", (req,res) => {
-    res.send("Handling product routes, e.g. search for products")
-})
-
-module.exports = router
