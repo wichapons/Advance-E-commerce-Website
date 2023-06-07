37c82523054fbeed5c4103684f988073956a0fdb
Wichapon V <wichapon@gmail.com>
2023-06-07 17:52:54 +0700

    Add getProducts route to productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,29 @@
-const Product = require("../models/ProductModel")
+const Product = require("../models/ProductModel");
+const recordsPerPage = require("../config/pagination");
 
-const getProducts = (req, res) => {
-    const product = new Product;
-    product.name = "Topazio"
-    res.send("Handling product routes, e.g. search for products")
-}
+const getProducts = async (req, res, next) => {
+  try {
+    let pageNum = Number(req.query.pageNum); //get value after ? sign
+    const totalProducts = await Product.countDocuments({});
+    if (!pageNum) {
+      //if no page num requested set to 1 instead
+      pageNum = 1;
+    }
+
+    const products = await Product.find({})
+      .skip(recordsPerPage * (pageNum - 1))
+      .sort({ name: 1 })
+      .limit(recordsPerPage);
+    res.json({
+      products,
+      pageNum,
+      paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
-    getProducts:getProducts
-}
+  getProducts: getProducts,
+};
