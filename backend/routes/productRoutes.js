93c18107aeabf13c6f06f0211e3c37b70c60bb36
Wichapon V <wichapon@gmail.com>
2023-06-11 01:37:26 +0700

    Add routes for product search and search within category

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const productController = require('../controllers/productController')
 
-router.get("/category/:categoryName", productController.getProducts)
 router.get("/",productController.getProducts)
+router.get("/category/:categoryName", productController.getProducts)
+router.get("/search/:searchTextQuery", productController.getProducts)
+router.get("/category/:categoryName/search/:searchTextQuery", productController.getProducts)
 
 module.exports = router
