740cd8b64b146fa086391c031c2ef7a6fa4c7ef5
Wichapon V <wichapon@gmail.com>
2023-06-10 23:55:22 +0700

    Add route for product category retrieval

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const productController = require('../controllers/productController')
 
+router.get("/category/:categoryName", productController.getProducts)
 router.get("/",productController.getProducts)
 
 module.exports = router
