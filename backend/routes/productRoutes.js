31d501bbdacdcba6011fbe0cd1b9f2a1cd93d869
Wichapon V <wichapon@gmail.com>
2023-06-12 01:03:48 +0700

    Add product routes and admin routes for productController

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,9 +2,19 @@ const express = require('express')
 const router = express.Router()
 const productController = require('../controllers/productController')
 
+//Product routes
 router.get("/",productController.getProducts)
 router.get("/category/:categoryName", productController.getProducts)
 router.get("/search/:searchTextQuery", productController.getProducts)
 router.get("/category/:categoryName/search/:searchTextQuery", productController.getProducts)
+router.get("/bestsellers", productController.getBestsellers)
+router.get("/get-one/:id", productController.getProductById)
+
+// admin routes
+router.get("/admin", productController.adminGetProducts)
+router.delete("/admin/delete/:id", productController.adminDeleteProduct)
+router.post("/admin/create", productController.adminCreateProduct)
+router.put("/admin/update/:id", productController.adminUpdateProduct)
+router.post("/admin/upload", productController.adminUpload)
 
 module.exports = router
