12a8d402a24be7e96c753a73ca180870694bcb9c
Wichapon V <wichapon@gmail.com>
2023-06-13 13:44:20 +0700

    Add generateAuthToken utility and set cookie to client on user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/UserModel");
 const { hashPassword } = require("../utils/hashPassword");
+const generateAuthToken = require("../utils/generateAuthToken");
 
 
 const getUsers = async (req, res, next) => {
@@ -13,15 +14,17 @@ const getUsers = async (req, res, next) => {
 
 const registerUser = async (req, res, next) => {
     try {
-      const { name, lastName, email, password } = req.body;
+      const { name, lastName, password } = req.body;
+      let{email} = req.body;
       if (!(name && lastName && email && password)) {
         return res.status(400).send("All inputs are required");
       }
-  
-      const userExists = await User.findOne({ email });
+      email = email.toLowerCase();
+      const userExists = await User.findOne({email});
+      console.log("userExists = ",userExists);
       //check if user exists
       if (userExists) {
-        return res.status(400).json({ error: "user already existed" });
+        return res.status(400).send({ error: "user already existed" });
       } else {
 
         const hashedPassword =  hashPassword(password)
@@ -32,8 +35,26 @@ const registerUser = async (req, res, next) => {
           email: email.toLowerCase(),//change email to lower case
           password: hashedPassword
         });
-        res.status(201).send(user); 
-      }
+        res
+        .cookie("access_token", generateAuthToken(user._id, user.name, user.lastName, user.email, user.isAdmin), { //send cookie to client
+            //cookie parameter
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "strict"
+      })
+        .status(201)
+        .json({
+          success: "User created",
+          userCreated: {
+            _id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+          },
+        });
+    }
+
     } catch (err) {
       next(err);
     }
