3c339ff76e8e48450abaa78931cffdffb5a93346
Wichapon V <wichapon@gmail.com>
2023-06-13 23:36:10 +0700

    Add updateUserProfile route for user profile updates

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -90,9 +90,44 @@ const registerUser = async (req, res, next) => {
     }
   };
 
+  const updateUserProfile = async (req, res, next) => {
+    try {
+      const user = await User.findById(req.user._id).orFail();
+      user.name = req.body.name || user.name;
+      user.lastName = req.body.lastName || user.lastName;
+      user.email = req.body.email || user.email;
+      user.phoneNumber = req.body.phoneNumber;
+      user.address = req.body.address;
+      user.country = req.body.country;
+      user.zipCode = req.body.zipCode;
+      user.city = req.body.city;
+      user.state = req.body.state;
+      
+      //check password match on DB
+      if (req.body.password !== user.password) {
+        user.password = hashPassword(req.body.password);
+      }
+      await user.save();
+  
+      res.json({
+        success: "user updated",
+        userUpdated: {
+          _id: user._id,
+          name: user.name,
+          lastName: user.lastName,
+          email: user.email,
+          isAdmin: user.isAdmin,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+  
 
 module.exports = {
     getUsers:getUsers,
     registerUser:registerUser,
     loginUser:loginUser,
+    updateUserProfile:updateUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const userController = require("../controllers/userController")
+const verifyAuthToken = require("../middlewares/verifyAuthToken")
 
-// user logged in routes:
 router.post("/register", userController.registerUser)
 router.post("/login", userController.loginUser)
 
+// user routes:
+router.use(verifyAuthToken.verifyIsLoggedIn);
+router.put("/profile", userController.updateUserProfile);
+
 // admin routes:
+router.use(verifyAuthToken.verifyIsAdmin);
 router.get("/", userController.getUsers)
 
 
