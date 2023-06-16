eebe41c4ffff4d426ebf64718a42f9187cc92145
Wichapon V <wichapon@gmail.com>
2023-06-16 12:07:38 +0700

    Add routes and controller for GET POST PUT user orders and GET PUT order details and status for admin also for analytics dashboard

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,123 @@
+const Order = require("../models/OrderModel");
+const ObjectId = require("mongodb").ObjectId;
 
-const getOrders = (req, res) => {
-    res.send("Handling order routes, e.g. get orders")
+const getUserOrders = async (req, res, next) => {
+    try {
+        const orders = await Order.find({ user: new ObjectId(req.user._id) });
+        res.send(orders);
+    } catch (err) {
+        next(err)
+    }
 }
-module.exports = getOrders
+
+const getOrderWithUserData = async (req, res, next) => {
+    try {
+       const order = await Order.findById(req.params.id).populate("user", "-password -isAdmin -_id -__v -createdAt -updatedAt").orFail();
+        res.send(order);
+    } catch (err) {
+        next(err)
+    }
+}
+
+const createOrder = async (req, res, next) => {
+    try {
+        const { cartItems, orderTotal, paymentMethod } = req.body;
+        if (!cartItems || !orderTotal || !paymentMethod) {
+            return res.status(400).send("All inputs are required");
+        }
+
+        let arrayOfProductsId = cartItems.map((item) => {
+            return item.productID;
+        })
+
+        let ArrayOfqty = cartItems.map((item) => {
+            return Number(item.quantity);
+        })
+
+        await Product.find({ _id: { $in: arrayOfProductsId } }).then((products) => {
+            products.forEach(function (product, idx) {
+                product.sales += ArrayOfqty[idx]; //increase sales for each product 
+                product.save();
+            })
+        })
+
+        const order = new Order({
+            user: new ObjectId(req.user._id),
+            orderTotal: orderTotal,
+            cartItems: cartItems,
+            paymentMethod: paymentMethod,
+        })
+        const createdOrder = await order.save();
+        res.status(201).send(createdOrder);
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+
+const updateOrderToPaid = async (req, res, next) => {
+    try {
+        const order = await Order.findById(req.params.id).orFail();
+        order.isPaid = true;
+        order.paidAt = Date.now();
+
+        const updatedOrder = await order.save();
+        res.send(updatedOrder);
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+const updateOrderToDelivered = async (req, res, next) => {
+    try {
+       const order = await Order.findById(req.params.id).orFail();
+        order.isDelivered = true;
+        order.deliveredAt = Date.now();
+        const updatedOrder = await order.save();
+        res.send(updatedOrder);
+    } catch (err) {
+        next(err);
+    }
+}
+
+const getAllOrdersAdmin = async (req, res, next) => {
+    try {
+        const orders = await Order.find({}).populate("user","-password").sort({ paymentMethod: "desc" });
+        res.send(orders);
+    } catch (err) {
+        next(err)
+    }
+}
+
+const getOrderForAnalysis = async (req, res, next) => {
+    try {
+        const start = new Date(req.params.date);
+        start.setHours(0, 0, 0, 0); //start from 0 am
+        const end = new Date(req.params.date);
+        end.setHours(23, 59, 59, 999); // end at 11.59pm
+
+        const order = await Order.find({
+            createdAt: {
+                $gte: start,
+                $lte: end,
+            }
+        }).sort({ createdAt: "asc" });
+        res.send(order);
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+
+module.exports = {
+    getUserOrders:getUserOrders,
+    getOrderWithUserData:getOrderWithUserData,
+    createOrder:createOrder,
+    updateOrderToPaid:updateOrderToPaid,
+    updateOrderToDelivered:updateOrderToDelivered,
+    getAllOrdersAdmin:getAllOrdersAdmin,
+    getOrderForAnalysis:getOrderForAnalysis
+}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const getOrders = require("../controllers/orderController")
+const verifyAuthToken = require('../middlewares/verifyAuthToken')
+const orderController = require("../controllers/orderController")
 
-router.get("/", getOrders)
+// user routes
+router.use(verifyAuthToken.verifyIsLoggedIn) 
+router.get("/", orderController.getUserOrders)
+router.get("/user/:id", orderController.getOrderWithUserData);
+router.post("/", orderController.createOrder);
+router.put("/paid/:id", orderController.updateOrderToPaid);
+
+
+// admin routes
+router.use(verifyAuthToken.verifyIsAdmin)
+router.put("/delivered/:id", orderController.updateOrderToDelivered);
+router.get("/admin", orderController.getAllOrdersAdmin);
+router.get("/analysis/:date", orderController.getOrderForAnalysis);
 
 module.exports = router
