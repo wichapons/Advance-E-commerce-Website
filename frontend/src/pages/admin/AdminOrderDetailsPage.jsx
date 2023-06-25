4cf51eb31b37cf65ea7038a11422a15c6a46105b
Wichapon V <wichapon@gmail.com>
2023-06-26 00:49:13 +0700

    Add Axios API call to retrieve order for AdminOrderDetailsPage

--- frontend/src/pages/admin/AdminOrderDetailsPage.jsx
+++ frontend/src/pages/admin/AdminOrderDetailsPage.jsx
@@ -1,9 +1,13 @@
-import OrderDetailsPageComponent from "./components/OrderDetailsPageComponent";
+import OrderDetailsPageComponent from './components/OrderDetailsPageComponent'
+import axios from "axios";
+
+const getOrder = async(id) => {
+    const response = await axios.get("/api/orders/user/" + id);
+    return response.data;
+}
 
 const AdminOrderDetailsPage = () => {
-  return(
-    <OrderDetailsPageComponent />
-);
+  return (<OrderDetailsPageComponent getOrder={getOrder} />)
 };
 
 export default AdminOrderDetailsPage;
