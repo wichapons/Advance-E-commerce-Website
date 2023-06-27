97a8f1a64ea7e8f937b0c959a466c7ed39430fad
Wichapon V <wichapon@gmail.com>
2023-06-27 23:31:08 +0700

    Refactor LoginPage to use LoginPageComponent, add local state for controlling login spining animation and wrong credentials alert

---
+++ frontend/src/pages/components/LoginPageComponent.jsx
@@ -0,0 +1,101 @@
+import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Spinner from 'react-bootstrap/Spinner'
+
+const LoginPageComponent = ({loginUserApiRequest}) => {
+  const [validated, setValidated] = useState(false);
+  const [loginUserResponseState, setLoginUserResponseState] = useState({
+    success: "",
+    error: "",
+    loading: false,
+  });
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    const element = form.elements;
+    const email = element.email.value;
+    const password = element.password.value;
+    const doNotLogout = element.doNotLogout.checked;
+
+    event.preventDefault(); //prevent page refresh after send request
+
+    //login request logic
+    if (form.checkValidity() === true && email && password) {
+      setLoginUserResponseState({ loading: true });
+      loginUserApiRequest(email, password, doNotLogout)
+      .then((res) => {
+        console.log(res);
+        setLoginUserResponseState({ success: res.success, loading: false, error: "" });
+      })
+      .catch((err) => {
+        setLoginUserResponseState({ error: err.response.data.message ? err.response.data.message : err.response.data })
+      });
+  }
+    setValidated(true);
+  };
+  return (
+    <Container>
+      <Row className="mt-5 justify-content-md-center">
+        <Col md={6}>
+          <h1>Login</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>            
+            
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                name="email"
+                required
+                type="email"
+                placeholder="Enter email"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name="password"
+                required
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check
+                name="doNotLogout"
+                type="checkbox"
+                label="Do not logout"
+              />
+            </Form.Group>
+
+            <Row className="pb-2">
+              <Col>
+               Don't you have an account?
+                <Link to={"/register"}> Register </Link>
+              </Col>
+            </Row>
+
+            <Button variant="primary" type="submit">
+            {/* control the spinner at login button */}
+              {loginUserResponseState && loginUserResponseState.loading === true ? (
+                <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              ) : ""}
+              Login
+            </Button>
+            <Alert show={loginUserResponseState && loginUserResponseState.error ==="wrong credentials"} variant="danger">
+                Wrong credentials
+            </Alert>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default LoginPageComponent;
+
