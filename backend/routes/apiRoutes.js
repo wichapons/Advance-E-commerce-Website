3cfba1875fba57fdadec1e3267c435354ddabfe6
Wichapon V <wichapon@gmail.com>
2023-06-28 15:29:28 +0700

    Add JWT token authentication to get-token route

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -4,6 +4,18 @@ const productRoutes = require("./productRoutes")
 const categoryRoutes = require("./categoryRoutes")
 const userRoutes = require("./userRoutes")
 const orderRoutes = require("./orderRoutes")
+const jwt = require("jsonwebtoken");
+
+app.get("/get-token", (req, res) => {
+    try {
+        const cookie = req.cookies;
+        const accessToken = cookie.access_token;
+        const decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
+        return res.json({ token: decoded.name, isAdmin: decoded.isAdmin });
+    } catch (err) {
+        return res.status(401).send("Unauthorized. Invalid Token");
+    }
+})
 
 app.use("/products", productRoutes)
 app.use("/categories", categoryRoutes)
