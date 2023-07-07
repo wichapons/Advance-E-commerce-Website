645a2832ba4a90d5bb73e59d34713dae607a5368
Wichapon V <wichapon@gmail.com>
2023-07-07 16:51:56 +0700

    Update routes, Add edit user page functionality for admin, and add PUT/GET API request to update and fetch user data

--- frontend/src/pages/admin/AdminEditUserPage.jsx
+++ frontend/src/pages/admin/AdminEditUserPage.jsx
@@ -1,54 +1,23 @@
-import {Row,Col,Container,Form,Button,Table} from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import AdminEditUserPageComponent from "./components/EditUserPageComponent";
+import axios from "axios";
+
+//update user data in database
+const updateUserApiRequest = async (userId, name, lastName, email, isAdmin) => {
+  const { data } = await axios.put(`/api/users/${userId}`, { name, lastName, email, isAdmin });
+    return data;
+}
+
+//get user data in database
+const fetchUser = async (userId) => {
+  const { data } = await axios.get(`/api/users/${userId}`);
+  return data;
+}
+
 
 const AdminEditUserPage = () => {
-  const [validated, setValidated] = useState(false);
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-    }
-    setValidated(true);
-  };
-
+  
   return (
-    <Container>
-      <Row className="justify-content-md-center mt-5">
-        <Col md={1}>
-          <Link to="/admin/products" className="btn btn-info my-3">
-            Go Back
-          </Link>
-        </Col>
-
-        <Col md={6}>
-          <h1>Edit user details</h1>
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control name="name" required type="text" defaultValue="" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicLastName">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control name="lastName" required type="text" defaultValue= ""/>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCount">
-              <Form.Label>Email</Form.Label>
-              <Form.Control name="email" required type="email" defaultValue="" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check name="isAdmin" type="checkbox" label="Is admin" />
-            </Form.Group>
-            
-            <Button variant="primary" type="submit">
-              Update
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <AdminEditUserPageComponent updateUserApiRequest={updateUserApiRequest} fetchUser={fetchUser} />
   );
 };
 
