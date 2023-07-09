239959819ae53dd820ddcc5a7b462deabe526879
Wichapon V <wichapon@gmail.com>
2023-07-09 23:24:09 +0700

    Add function to show to the order charts that fetching data from database

--- frontend/src/pages/admin/AdminAnalyticsPage.jsx
+++ frontend/src/pages/admin/AdminAnalyticsPage.jsx
@@ -1,8 +1,31 @@
 import AnalyticsPageComponent from "./components/AnalyticsPageComponent";
+import axios from "axios";
+
+const fetchOrdersForFirstDate = async (abctrl, firstDateToCompare) => {
+  // Fetch orders data for the first date to compare
+  try{
+    const response = await axios.get("/api/orders/analysis/" + firstDateToCompare, {
+      signal: abctrl.signal,
+    });
+    return response.data;
+  }catch(err){
+    console.log('err',err);
+    return;
+  } 
+}
+
+const fetchOrdersForSecondDate = async (abctrl, secondDateToCompare) => {
+  // Fetch orders data for the second date to compare
+  const response = await axios.get("/api/orders/analysis/" + secondDateToCompare, {
+    signal: abctrl.signal,
+  });
+  return response.data;
+};
+
 
 const AdminAnalyticsPage = () => {
   return (
-    <AnalyticsPageComponent />
+    <AnalyticsPageComponent fetchOrdersForFirstDate={fetchOrdersForFirstDate} fetchOrdersForSecondDate={fetchOrdersForSecondDate} />
   )
 };
 
--- frontend/src/pages/admin/components/AnalyticsPageComponent.jsx
+++ frontend/src/pages/admin/components/AnalyticsPageComponent.jsx
@@ -1,17 +1,34 @@
 import { Row, Col, Form } from "react-bootstrap";
 import AdminLinksComponent from "../../../components/admin/AdminLinksComponent";
-import {LineChart,Line,XAxis,YAxis,CartesianGrid,Tooltip,Legend,ResponsiveContainer}from "recharts";
-import { useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { useState, useEffect } from "react";
 
-const AdminAnalyticsPageComponent = () => {
+const AdminAnalyticsPageComponent = ({
+  fetchOrdersForFirstDate,
+  fetchOrdersForSecondDate,
+}) => {
+  let previousDay = new Date();
+  previousDay.setDate(previousDay.getDate() - 1);
+
   const [firstDateToCompare, setFirstDateToCompare] = useState(
     new Date().toISOString().substring(0, 10)
   );
-  let previousDay = new Date();
-  previousDay.setDate(previousDay.getDate() - 1);
+
   const [secondDateToCompare, setSecondDateToCompare] = useState(
     new Date(previousDay).toISOString().substring(0, 10)
   );
+
+  const [dataForFirstSet, setDataForFirstSet] = useState([]);
+  const [dataForSecondSet, setDataForSecondSet] = useState([]);
 
   const firstDateHandler = (e) => {
     setFirstDateToCompare(e.target.value);
@@ -21,135 +38,74 @@ const AdminAnalyticsPageComponent = () => {
     setSecondDateToCompare(e.target.value);
   };
 
-  const data = [
-    {
-      name: "12:00 AM",
-      2022: 4000,
-      2021: 4100,
-    },
-    {
-      name: "1:00 AM",
-      2022: 4200,
-      2021: 4300,
-    },
-    {
-      name: "2:00 AM",
-      2022: 4400,
-      2021: 4500,
-    },
-    {
-      name: "3:00 AM",
-      2022: 4600,
-      2021: 4600,
-    },
-    {
-      name: "4:00 AM",
-      2022: 4800,
-      2021: 5000,
-    },
-    {
-      name: "5:00 AM",
-      2022: 5000,
-      2021: 5200,
-    },
-    {
-      name: "6:00 AM",
-      2022: 5200,
-      2021: 5400,
-    },
-    {
-      name: "7:00 AM",
-      2022: 5600,
-      2021: 6000,
-    },
-    {
-      name: "8:00 AM",
-      2022: 6000,
-      2021: 6300,
-    },
-    {
-      name: "9:00 AM",
-      2022: 6400,
-      2021: 7000,
-    },
-    {
-      name: "10:00 AM",
-      2022: 6800,
-      2021: 7200,
-    },
-    {
-      name: "11:00 AM",
-      2022: 7000,
-      2021: 7800,
-    },
-    {
-      name: "12:00 PM",
-      2022: 7200,
-      2021: 8200,
-    },
-    {
-      name: "1:00 PM",
-      2022: 7500,
-      2021: 8400,
-    },
-    {
-      name: "2:00 PM",
-      2022: 7700,
-      2021: 9000,
-    },
-    {
-      name: "3:00 PM",
-      2022: 8000,
-      2021: 9500,
-    },
-    {
-      name: "4:00 PM",
-      2022: 8400,
-      2021: 10000,
-    },
-    {
-      name: "5:00 PM",
-      2022: 9000,
-      2021: 12000,
-    },
-    {
-      name: "6:00 PM",
-      2022: 10500,
-      2021: 17000,
-    },
-    {
-      name: "7:00 PM",
-      2022: 16000,
-      2021: 20000,
-    },
-    {
-      name: "8:00 PM",
-      2022: 17000,
-      2021: 21000,
-    },
-    {
-      name: "9:00 PM",
-      2022: 17400,
-      2021: 22000,
-    },
-    {
-      name: "10:00 PM",
-      2021: 23000,
-      2022: 19500,
-    },
-    {
-      name: "11:00 PM",
-      2021: 23500,
-      2022: 21000,
-    },
-  ];
+  useEffect(() => {
+    // Create an abort controller instance
+    const abctrl = new AbortController();
+
+    // Fetch orders data for the first date to compare
+    fetchOrdersForFirstDate(abctrl, firstDateToCompare)
+      .then((data) => {
+        console.log(data);
+        let orderSum = 0;
+        // Process the fetched orders data
+        const orders = data.map((order) => {
+          //sum up orders
+          orderSum += order.orderTotal.cartSubtotal;
+          // Format the date
+          let date = new Date(order.createdAt).toLocaleString("en-US", {
+            hour: "numeric",
+            hour12: true,
+            timeZone: "UTC",
+          });
+          // Return an object with the formatted date and order sum for the first date
+          return { name: date, [firstDateToCompare]: orderSum };
+        });
+        setDataForFirstSet(orders);
+      })
+      .catch((er) =>
+        console.log(
+          er.response.data.message ? er.response.data.message : er.response.data
+        )
+      );
+
+    // Fetch orders data for the second date to compare
+    fetchOrdersForSecondDate(abctrl, secondDateToCompare)
+      .then((data) => {
+        let orderSum = 0;
+        // Process the fetched orders data
+        const orders = data.map((order) => {
+          orderSum += order.orderTotal.cartSubtotal;
+          // Format the date
+          let date = new Date(order.createdAt).toLocaleString("en-US", {
+            hour: "numeric",
+            hour12: true,
+            timeZone: "UTC",
+          });
+          // Return an object with the formatted date and order sum for the second date
+          return { name: date, [secondDateToCompare]: orderSum };
+        });
+        setDataForSecondSet(orders);
+      })
+      .catch((er) =>
+        console.log(
+          er.response.data.message ? er.response.data.message : er.response.data
+        )
+      );
+
+    // Clean up function to abort the fetch if the component unmounts or the dependency values change
+    return () => abctrl.abort();
+  }, [firstDateToCompare, secondDateToCompare]);
+
   return (
     <Row className="m-5">
       <Col md={2}>
         <AdminLinksComponent />
       </Col>
       <Col md={10}>
-        <h1>Black Friday Cumulative Revenue {firstDateToCompare} VS {secondDateToCompare}</h1>
+        <h1>
+          Black Friday Cumulative Revenue {firstDateToCompare} VS{" "}
+          {secondDateToCompare}
+        </h1>
         <Form.Group controlId="firstDateToCompare">
           <Form.Label>Select First Date To Compare</Form.Label>
           <Form.Control
@@ -173,7 +129,6 @@ const AdminAnalyticsPageComponent = () => {
         </Form.Group>
         <ResponsiveContainer width="100%" height={500}>
           <LineChart
-            data={data}
             margin={{
               top: 5,
               right: 30,
@@ -196,19 +151,43 @@ const AdminAnalyticsPageComponent = () => {
             />
             <Tooltip />
             <Legend verticalAlign="top" height={36} />
-            <Line
-              type="monotone"
-              dataKey="2021"
-              stroke="#8884d8"
-              activeDot={{ r: 8 }}
-              strokeWidth={4}
-            />
-            <Line
-              type="monotone"
-              dataKey="2022"
-              stroke="#82ca9d"
-              strokeWidth={4}
-            />
+            {dataForFirstSet.length > dataForSecondSet.length ? (
+              <>
+                <Line
+                  data={dataForFirstSet}
+                  type="monotone"
+                  dataKey={firstDateToCompare}
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                  strokeWidth={4}
+                />
+                <Line
+                  data={dataForSecondSet}
+                  type="monotone"
+                  dataKey={secondDateToCompare}
+                  stroke="#82ca9d"
+                  strokeWidth={4}
+                />
+              </>
+            ) : (
+              <>
+                <Line
+                  data={dataForSecondSet}
+                  type="monotone"
+                  dataKey={secondDateToCompare}
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                  strokeWidth={4}
+                />
+                <Line
+                  data={dataForFirstSet}
+                  type="monotone"
+                  dataKey={firstDateToCompare}
+                  stroke="#82ca9d"
+                  strokeWidth={4}
+                />
+              </>
+            )}
           </LineChart>
         </ResponsiveContainer>
       </Col>
