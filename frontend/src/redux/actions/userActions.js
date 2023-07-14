e1095c16f5594ff4a31273e28dfc2a0777585e5f
Wichapon V <wichapon@gmail.com>
2023-07-14 15:34:47 +0700

    Fix bug: Cookies are not being deleted after users logout.

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -15,5 +15,11 @@ export const logout = () => (dispatch) => {
     localStorage.removeItem("userInfo");
     sessionStorage.removeItem("userInfo");
     localStorage.removeItem("cart");
+    const clearCookie = (name) => {
+        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+      };
+      
+      // Usage: Call clearCookie with the name of the cookie you want to clear
+      clearCookie("access_token");      
     dispatch({ type: LOGOUT_USER })
 }
