4fcfb362da2356681c3eeafa2137ae950eec36c5
Wichapon V <wichapon@gmail.com>
2023-08-01 02:08:09 +0700

    Change initial  price value

--- frontend/src/pages/components/ProductListPageComponent.jsx
+++ frontend/src/pages/components/ProductListPageComponent.jsx
@@ -19,7 +19,7 @@ const ProductListPageComponent = ({ getProducts, categories }) => {
   const [attrsFromFilter, setAttrsFromFilter] = useState([]); // collect user filters for category attributes
   const [showResetFiltersButton, setShowResetFiltersButton] = useState(false);
   const [filters, setFilters] = useState({});
-  const [price, setPrice] = useState(500);
+  const [price, setPrice] = useState(1000);
   const [ratingsFromFilter, setRatingsFromFilter] = useState({});
   const [categoriesFromFilter, setCategoriesFromFilter] = useState({});
   const [sortOption, setSortOption] = useState("");
