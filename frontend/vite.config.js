e493dd660db9841699ec833546f74f5a1ecd10a6
Wichapon Viriyatharangkurn <wichapon@gmail.com>
2025-06-05 00:40:23 +0700

    Revert "Refactor Vite config to load environment variables for API base URL"
    This reverts commit 506c2fa2532350ef1c69a7930ac0b0fc5db517e7.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,17 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    plugins: [react()],
-    server: {
-      port: '3000',
-      proxy: {
-        '/api': {
-          target: env.VITE_API_BASE_URL,
-          changeOrigin: true,
-        }
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: '3000',
+    proxy: {
+      '/api': {
+        target: "https://topazio-shop-backend.onrender.com",
+        changeOrigin: true,
+        //rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
