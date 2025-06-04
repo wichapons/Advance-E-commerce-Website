506c2fa2532350ef1c69a7930ac0b0fc5db517e7
Wichapon Viriyatharangkurn <wichapon@gmail.com>
2025-06-04 21:27:59 +0700

    Refactor Vite config to load environment variables for API base URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: '3000',
-    proxy: {
-      '/api': {
-        target: "https://topazio-shop-backend.onrender.com",
-        changeOrigin: true,
-        //rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: '3000',
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        }
       }
     }
   }
